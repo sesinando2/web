e81bcd4fac22a241f75e93e8873abd7802cee0f4
Nando <sesinando2@gmail.com>
2016-08-15 21:12:50 +1000

    Added ng-sanitize, Navbar styling.

--- client/app/common/navigation/navigation.controller.js
+++ client/app/common/navigation/navigation.controller.js
@@ -1,7 +1,12 @@
+import logo from './logo.png';
+
 class NavigationController {
 
-  constructor() {
+  /* ngInject */
+  constructor(userInfo) {
     this.name = 'navigation';
+    this.logo = logo;
+    this.username = userInfo.name;
   }
 }
 
--- client/app/common/navigation/navigation.js
+++ client/app/common/navigation/navigation.js
@@ -1,9 +1,12 @@
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
+import ngSanitize from 'angular-sanitize';
 import navigationComponent from './navigation.component';
 
+import './logo.png';
+
 let navigationModule = angular.module('navigation', [
-  uiRouter
+  uiRouter, ngSanitize, 'config'
 ])
 
 .component('navigation', navigationComponent)
